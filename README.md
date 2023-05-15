c32c5c3c135c769e2ec861563ba9753f79243456
whk06 <wang_kaihan@163.com>
2023-05-15 13:48:30 +0800

    first main commit

--- README.md
+++
@@ -1,5 +0,0 @@
-# minit-learning-demo
-
-Learn the code of [minit](https://github.com/YaleGuo/minit)
-
-The branch named "main" is always useful.
---
+++ src/server/test.java
@@ -0,0 +1,13 @@
+package src.server;
+
+import java.io.File;
+
+public class test {
+
+    public static final String WEB_ROOT =
+            System.getProperty("user.dir") + File.separator + "webroot";
+
+    public static void main(String[] args) {
+        System.out.println(WEB_ROOT);
+    }
+}
