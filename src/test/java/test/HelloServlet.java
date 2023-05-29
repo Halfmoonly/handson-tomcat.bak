2a89aa9e0db8da12b1c7132bf14dcab3b98198a1
whk06 <wang_kaihan@163.com>
2023-05-29 14:56:22 +0800

    commit geek_chapter03: import javax.servlet.Servlet

--- src/test/java/test/HelloServlet.java
+++ src/test/java/test/HelloServlet.java
@@ -1,20 +1,42 @@
 package test;
 
-import server.Request;
-import server.Response;
-import server.Servlet;
-
 import java.io.IOException;
 
-public class HelloServlet implements Servlet {
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class HelloServlet implements Servlet{
+
     @Override
-    public void service(Request req, Response res) throws IOException {
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.setCharacterEncoding("UTF-8");
+
         String doc = "<!DOCTYPE html> \n" +
                 "<html>\n" +
                 "<head><meta charset=\"utf-8\"><title>Test</title></head>\n"+
                 "<body bgcolor=\"#f0f0f0\">\n" +
                 "<h1 align=\"center\">" + "Hello World 你好" + "</h1>\n";
+        res.getWriter().println(doc);
+    }
 
-        res.getOutput().write(doc.getBytes("utf-8"));
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void init(ServletConfig arg0) throws ServletException {
     }
 }
