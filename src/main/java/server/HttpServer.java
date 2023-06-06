2cdbd4067be82f6f96c24ad41249e277b86fcc61
whk06 <wang_kaihan@163.com>
2023-06-06 10:15:28 +0800

    commit geek_chapter03: add HttpConnector.java and HttpProcessor.java

--- src/main/java/server/HttpServer.java
+++ src/main/java/server/HttpServer.java
@@ -1,61 +1,14 @@
 package server;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class HttpServer {
-    public static final String WEB_ROOT = System.getProperty("user.dir") + File.separator + "webroot";
+    public static final String WEB_ROOT =
+            System.getProperty("user.dir") + File.separator + "webroot";
 
     public static void main(String[] args) {
-        HttpServer server = new HttpServer();
-        server.await();
+        HttpConnector connector = new HttpConnector();
+        connector.start();
     }
 
-    public void await() {
-        ServerSocket serverSocket = null;
-        int port = 8080;
-        try {
-            serverSocket = new ServerSocket(port, 1, InetAddress.getByName("127.0.0.1"));
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-
-        while (true) {
-            Socket socket = null;
-            InputStream input = null;
-            OutputStream output = null;
-            try {
-                socket = serverSocket.accept();
-                input = socket.getInputStream();
-                output = socket.getOutputStream();
-
-                // create Request object and parse
-                Request request = new Request(input);
-                request.parse();
-
-                // create Response object
-                Response response = new Response(output);
-                response.setRequest(request);
-
-                if (request.getUri().startsWith("/servlet/")) {
-                    ServletProcessor processor = new ServletProcessor();
-                    processor.process(request, response);
-                }
-                else {
-                    StaticResourceProcessor processor = new StaticResourceProcessor();
-                    processor.process(request, response);
-                }
-                // close the socket
-                socket.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }
